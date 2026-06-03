8deea2bc7853fc176126255b275d01b04fd0d759
ayada <ayadalharbi232@gmail.com>
2026-06-03 20:22:00 +0100

    Added updatePerspective to Renderable

--- Console3DRendering/src/rendering3D/Renderable.java
+++ Console3DRendering/src/rendering3D/Renderable.java
@@ -4,6 +4,8 @@ public interface Renderable {
 	
 	public void render();
 	
+	public void updatePerspective();
+	
 	// All renderable objects have an associated observer. 
 	public Observer getObserver();
 
