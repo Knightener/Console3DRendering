35c24a97cd180ffbde69c263e3a46f4f225616f9
ayada <ayadalharbi232@gmail.com>
2026-07-08 17:41:52 +0100

    Fixed getCuboid topology

--- Console3DRendering/src/building3D/Shape3D.java
+++ Console3DRendering/src/building3D/Shape3D.java
@@ -11,7 +11,8 @@ import texturing.TexturePresets;
 
 public class Shape3D {
 
-	public static Mesh getCuboid(Observer observer, R3Point cornerA, R3Point cornerB) {
+	// Each component of cornerA assumed to be smaller than cornerB
+	private static Mesh getCuboidAux(Observer observer, R3Point cornerA, R3Point cornerB) {
 		Texture texture = new Texture(TexturePresets.WHITE, 1);
 
 		List<R3Point> vertices = new ArrayList<>();
@@ -28,18 +29,34 @@ public class Shape3D {
 
 		Mesh cuboid = new Mesh(observer, vertices);
 
+		// Vertices ordered to ensure outward pointing normals. 
+		
 		// X fixed
 		cuboid.unrestrictedCreateFace(texture, 0b000, 0b001, 0b011, 0b010);
-		cuboid.unrestrictedCreateFace(texture, 0b100, 0b101, 0b111, 0b110);
+		cuboid.unrestrictedCreateFace(texture, 0b100, 0b110, 0b111, 0b101);
 		
 		// Y fixed
-		cuboid.unrestrictedCreateFace(texture, 0b000, 0b001, 0b101, 0b100);
+		cuboid.unrestrictedCreateFace(texture, 0b000, 0b100, 0b101, 0b001);
 		cuboid.unrestrictedCreateFace(texture, 0b010, 0b011, 0b111, 0b110);
 		
 		// Z fixed
 		cuboid.unrestrictedCreateFace(texture, 0b000, 0b010, 0b110, 0b100);
-		cuboid.unrestrictedCreateFace(texture, 0b001, 0b011, 0b111, 0b101);
+		cuboid.unrestrictedCreateFace(texture, 0b001, 0b101, 0b111, 0b011);
 		
-		return cuboid; 
+		return cuboid;
+	}
+
+	public static Mesh getCuboid(Observer observer, R3Point cornerA, R3Point cornerB) {
+		R3Point cornerMin = new R3Point(
+			Math.min(cornerA.getX(), cornerB.getX()),
+			Math.min(cornerA.getY(), cornerB.getY()), 
+			Math.min(cornerA.getZ(), cornerB.getZ()));
+
+		R3Point cornerMax = new R3Point(
+			Math.max(cornerA.getX(), cornerB.getX()),
+			Math.max(cornerA.getY(), cornerB.getY()), 
+			Math.max(cornerA.getZ(), cornerB.getZ()));
+		
+		return getCuboidAux(observer, cornerMin, cornerMax);
 	}
 }
